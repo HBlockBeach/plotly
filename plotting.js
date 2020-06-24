fe2ba20e007bde3c13d5de9ab7f98dfe4d8aa65e
Hunter Block <hunterblock@Hunters-MacBook-Pro.local>
2020-06-23 22:47:30 -0400

    push

--- plotting.js
+++ plotting.js
@@ -1,31 +1,89 @@
-//d3.json("samples.json").then(function(data){
-  //  console.log(data)
-//});
+function buildplots(input) {
 
+    d3.json("samples.json").then((data) => {
 
-
-d3.json("samples.json").then((data) => {
-    const samples = data.samples[0]
-    const id = samples.id
-    const otu = samples.otu_ids
-    const name = samples.otu_labels
-    const values = samples.sample_values
     
-    console.log(samples)
-    console.log(id)
-    console.log(otu)
-    console.log(name)
-    console.log(values)
+    var samples = data.samples;
+    var fortables = samples.filter(sample => sample.id == input)
+    var subject = fortables[0]
+
+    const id = subject.id
+    const otu = subject.otu_ids
+    const name = subject.otu_labels
+    const values = subject.sample_values
+    
+    var ystuff = otu.slice(0,10).map(otu=> `OTU ${otu}`).reverse()
+    var bardata = [{
+        y: ystuff,
+        x: values.slice(0,10).reverse(),
+        text: name.slice(0,10).reverse(),
+        type: "bar",
+        orientation: 'h'
+    }
+];
+
+    Plotly.newPlot("bar", bardata)
+    
+    var bubbledata = [{
+        x: otu,
+        y: values,
+        text: name,
+        mode: "markers",
+        marker: {
+            size: values,
+            color: otu
+        }
+    }];
+
+    Plotly.newPlot("bubble", bubbledata)
+    })
+};
+
+function buildmetadata(input) {
+    d3.json("samples.json").then((data) => {
+    const metadata = data.metadata;
+    var fordemos = metadata.filter(data => data.id == input);
+    var subject = fordemos[0]
+    
+    var panel = d3.select('#sample-metadata');
+    panel.html("")
+
+    Object.entries(subject).forEach(([cat,value]) => {
+
+        panel.append("h5").text(`${cat}: ${value}`)
+    })
+
+    })
+};
+
+
+
+
+function makeithappen() {
+
+    var grabid = d3.select("#selDataset");
+
+    d3.json("samples.json").then((data) =>{
+        var ids = data.names;
+
+    ids.forEach((number) => {
+        grabid
+            .append("option")
+            .text(number)
+            .property("value" , number)
+    })
+
+    var first =ids[0]
+    buildplots(first)
+    buildmetadata(first)
+
 });
-//var ystuff = otu.slice(0,10).map(otu=> `OTU ${otu}`).reverse()
-var bardata = [{
-    y: otu,
-    x: values,
-    text: name,
-    type: "bar",
-    orientation: 'h'
+}
 
-}];
+function updated(value) {
+    buildplots(value);
+    buildmetadata(value);
 
-Plotly.newPlot("bar", bardata)
+}
 
+makeithappen()
